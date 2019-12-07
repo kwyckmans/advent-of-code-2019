3ec9da82c2dfcb2d6c9a1d9a02c0a21910e26a6f
Kevin Wyckmans <kevin@wyckd.be>
2019-12-07 21:15:22 +0100

    add solution for day 3, part 1

--- day-three.py
+++ day-three.py
@@ -1,35 +1,13 @@
-DIAGRAM = [["." for _ in range(0, 11)] for _ in range(0, 11)]
+import math
 
-LOCATIONS = {}
-CROSSINGS = []
+POINTS = {}
+POINTS["first"] = set()
+POINTS["second"] = set()
 
-
-def swap(a, b):
-    temp = a
-    a = b
-    b = temp
-
-
-def draw_wire(icon="o"):
-    for location in LOCATIONS:
-        print(f"Drawing wire for location {location}")
-        start, end = location
-        start_x, start_y = start
-        end_x, end_y = end
-
-        print(start_x, start_y, end_x, end_y)
-        # if start_x > end_x: swap(start_x, start_y)
-        # if start_y > end_y: swap(start_y, end_y)
-
-        for x in range(start_x, end_x):
-            for y in range(start_y, end_y):
-                print("Setting {x},{y}")
-                DIAGRAM[y][x] = icon
-
+def manhattan_distance(location):
+    return abs(location[0] - 1) + abs(location[1] - 1)
 
 def plot_wire(directions, wire, marker="x"):
-    LOCATIONS[wire] = []
-
     port = (1, 1)
     loc = port
 
@@ -37,55 +15,61 @@ def plot_wire(directions, wire, marker="x"):
 
     direction, value = "R", 0
     for instruction in DIRS:
-        print(f"At location {loc}")
-
+    
         direction = instruction[0]
         val = int(instruction[1:])
-
-        print(f"direction {direction}, value {val}")
-
         if direction == "R":
             start = loc[0]
             end = loc[0] + val
+            y = loc[1]
 
-            LOCATIONS[wire].append(((start, loc[1]), (end, loc[1])))
+            for i in range(start + 1, end + 1):
+                POINTS[wire].add((i,y))
 
             loc = (end, loc[1])
 
         elif direction == "U":
             start = loc[1]
             end = loc[1] + val
-            LOCATIONS[wire].append(((loc[0], start), (loc[0], end)))
+            x = loc[0]
+
+            for i in range(start + 1, end + 1):
+                POINTS[wire].add((x,i))
 
             loc = (loc[0], end)
 
         elif direction == "L":
             start = loc[0]
             end = loc[0] - val
-            LOCATIONS[wire].append(((start, loc[1]), (end, loc[1])))
+            y = loc[1]
+
+            for i in range(start-1, end-1, -1):
+                POINTS[wire].add((i,y))
 
             loc = (end, loc[1])
 
         elif direction == "D":
             start = loc[1]
             end = loc[1] - val
-            LOCATIONS[wire].append(((loc[0], start), (loc[0], end)))
+            x = loc[0]
+
+            for i in range(start-1, end-1, -1):
+                POINTS[wire].add((x,i))
 
             loc = (loc[0], end)
 
         else:
             raise Exception("Impossible direction!", direction)
 
-
 if __name__ == "__main__":
     with open("day-three.txt", "r") as f:
         WIRES = [line for line in f]
 
+    print(f"Processing instructions: {WIRES[0]}")
     plot_wire(WIRES[0], "first")
+    print(f"Processing instructions: {WIRES[1]}")
+
     plot_wire(WIRES[1], "second")
 
-    for wire in LOCATIONS:
-        for location in LOCATIONS[wire]:
-            print(location)
-
-        print(f"{wire} finished")
+    distances = map(lambda loc: manhattan_distance(loc), POINTS["first"].intersection(POINTS["second"]))
+    print(min(distances))

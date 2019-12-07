f6621a1b6567d9a5c389d184da92b0947fadd3b9
Kevin Wyckmans <kevin@wyckd.be>
2019-12-07 23:31:46 +0100

    add solution for part two of day four

--- day-four.py
+++ day-four.py
@@ -1,3 +1,5 @@
+import re
+
 MIN = 128392
 MAX = 643281
 
@@ -19,14 +21,28 @@ def check_doubles(number):
 
     return False
 
+def check_exact_two_doubles(number):
+    num = str(number)
+
+    matches = re.findall("11+|22+|33+|44+|55+|66+|77+|88+|99+", num)
+
+    if matches and min([len(match) for match in matches]) == 2:
+        return True
+
+    return False
+
+
 if __name__ == "__main__":
-    test_cases = [11,22,33,44,55,66,77,88,99,112,211,121]
+    test_cases = [11,22,33,44,55,66,77,88,99,112,211,121, 1111,11112, 1111222344]
 
     for case in test_cases:
-        print(f"{case}: {check_doubles(case)}")
+        print(f"{case}: {check_exact_two_doubles(case)}")
 
     increasing = [num for num in range(MIN, MAX + 1) if check_monotonous_increase(num)]
-    print(len(increasing))
-
     increasing_with_doubles = [num for num in increasing if check_doubles(num)]
+    print(len(increasing_with_doubles))
+
+    
+    increasing = [num for num in range(MIN, MAX + 1) if check_monotonous_increase(num)]
+    increasing_with_doubles = [num for num in increasing if check_exact_two_doubles(num)]
     print(len(increasing_with_doubles))

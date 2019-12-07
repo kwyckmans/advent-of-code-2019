157de4599c3484ca288b3126c4af04be81970352
Kevin Wyckmans <kevin@wyckd.be>
2019-12-07 21:51:25 +0100

    add solution for day 3 - part 2

--- day-three.py
+++ day-three.py
@@ -4,8 +4,49 @@ POINTS = {}
 POINTS["first"] = set()
 POINTS["second"] = set()
 
+
+LOCATIONS = {}
+LOCATIONS["first"] = {}
+LOCATIONS["second"] = {}
+
+
+class Location:
+    def __init__(self, x, y, steps):
+        self._x = x
+        self._y = y
+        self._steps = steps
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def steps(self):
+        return self._steps
+
+    def __eq__(self, other):
+        if isinstance(other, Location):
+            return self.x == other.x and self.y == other.y
+
+        return NotImplemented
+
+    def __str__(self):
+        return f"({self.x}, {self.y} after {self.steps} steps)"
+
+    def __key(self):
+        return (self.x, self.y)
+
+    def __hash__(self):
+        return hash(self.__key())
+
+
 def manhattan_distance(location):
     return abs(location[0] - 1) + abs(location[1] - 1)
+
 
 def plot_wire(directions, wire, marker="x"):
     port = (1, 1)
@@ -14,8 +55,9 @@ def plot_wire(directions, wire, marker="x"):
     DIRS = directions.split(",")
 
     direction, value = "R", 0
+    steps = 1
     for instruction in DIRS:
-    
+
         direction = instruction[0]
         val = int(instruction[1:])
         if direction == "R":
@@ -24,7 +66,9 @@ def plot_wire(directions, wire, marker="x"):
             y = loc[1]
 
             for i in range(start + 1, end + 1):
-                POINTS[wire].add((i,y))
+                POINTS[wire].add((i, y))
+                LOCATIONS[wire][(i, y)] = steps
+                steps += 1
 
             loc = (end, loc[1])
 
@@ -34,7 +78,10 @@ def plot_wire(directions, wire, marker="x"):
             x = loc[0]
 
             for i in range(start + 1, end + 1):
-                POINTS[wire].add((x,i))
+                POINTS[wire].add((x, i))
+                LOCATIONS[wire][(x, i)] = steps
+
+                steps += 1
 
             loc = (loc[0], end)
 
@@ -43,8 +90,11 @@ def plot_wire(directions, wire, marker="x"):
             end = loc[0] - val
             y = loc[1]
 
-            for i in range(start-1, end-1, -1):
-                POINTS[wire].add((i,y))
+            for i in range(start - 1, end - 1, -1):
+                POINTS[wire].add((i, y))
+                LOCATIONS[wire][(i, y)] = steps
+
+                steps += 1
 
             loc = (end, loc[1])
 
@@ -53,13 +103,17 @@ def plot_wire(directions, wire, marker="x"):
             end = loc[1] - val
             x = loc[0]
 
-            for i in range(start-1, end-1, -1):
-                POINTS[wire].add((x,i))
+            for i in range(start - 1, end - 1, -1):
+                POINTS[wire].add((x, i))
+                LOCATIONS[wire][(x, i)] = steps
+
+                steps += 1
 
             loc = (loc[0], end)
 
         else:
             raise Exception("Impossible direction!", direction)
+
 
 if __name__ == "__main__":
     with open("day-three.txt", "r") as f:
@@ -71,5 +125,18 @@ if __name__ == "__main__":
 
     plot_wire(WIRES[1], "second")
 
-    distances = map(lambda loc: manhattan_distance(loc), POINTS["first"].intersection(POINTS["second"]))
-    print(min(distances))
+    distances = map(
+        lambda loc: manhattan_distance(loc),
+        POINTS["first"].intersection(POINTS["second"]),
+    )
+
+    print(f"Minimum distance is: {min(distances)}")
+
+    intersection_steps = []
+    for loc in LOCATIONS["first"]:
+        if loc in LOCATIONS["second"]:
+            intersection_steps.append(
+                LOCATIONS["first"][loc] + LOCATIONS["second"][loc]
+            )
+
+    print(f"Minimum number of steps: {min(intersection_steps)}")

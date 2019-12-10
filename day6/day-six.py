8f1bd4800863ac5f0061014139e68eb8649279b7
Kevin Wyckmans <kwyckmans@spotify.com>
2019-12-10 12:46:44 +0100

    add solution for 7.2

--- day6/day-six.py
+++ day6/day-six.py
@@ -1,3 +1,16 @@
+import queue
+
+# def search(graph, start):
+#     q = queue.Queue()
+#     start = (start, True)
+
+#     while not q.empty():
+#         v = q.get()
+#         if v[0] == 'SAN':
+#             return v
+        
+
+
 if __name__ == "__main__":
     orbits = {}
 
@@ -28,4 +41,11 @@ if __name__ == "__main__":
         total += len(orbits[orbit])
 
     print(total)
-    print(orbits["XQ6"])
+
+    # orbits["XQ6"]
+
+    my_orbit = orbits["YOU"]
+    santa_orbit = orbits["SAN"]
+
+    print(len(my_orbit.difference(santa_orbit)) + len(santa_orbit.difference(my_orbit)))
+    # print(orbits["XQ6"])
